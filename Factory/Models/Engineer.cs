de046bc393157c0374cf8c096bf1511d0326d157
John Diggins <rohnfiggins@gmail.com>
2023-03-18 13:00:59 -0700

    working on engineers/details

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -8,6 +8,6 @@ namespace Factory.Models
     public int EngineerId { get; set; }
     public string Name { get; set; }
     public List<Machine> Machines { get; set; }
-    //joinEntities?
+    public List<EngineerMachine> JoinEntities { get;}
   }
 }
