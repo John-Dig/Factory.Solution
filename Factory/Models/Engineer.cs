f767161351ac7dbea428c92d6fd3ed6d789c6eea
John Diggins <rohnfiggins@gmail.com>
2023-03-20 10:41:38 -0700

    restart project

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 
 namespace Factory.Models
 {
-  public class Engineer
-  {
-    public int EngineerId { get; set; }
-    public string Name { get; set; }
-    public List<Machine> Machines { get; set; }
-    public List<EnMa> JoinEntities { get;} //collection navigation property
-  }
+  // public class Engineer
+  // {
+  //   public int EngineerId { get; set; }
+  //   public string Name { get; set; }
+  //   public List<Machine> Machines { get; set; }
+  //   public List<EnMa> JoinEntities { get;} //collection navigation property
+  // }
 }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -4,14 +4,14 @@ using System.ComponentModel.DataAnnotations;
 namespace Factory.Models
 {
   public class Machine
-  {
-    public int MachineId { get; set; }
-    [Required(ErrorMessage = "The machine's name can't be empty!")]
-    public string Name { get; set; }
-    [Range(1, int.MaxValue, ErrorMessage = "You must add your machine to a engineer. Have you created a engineer yet?")]
-    public int EngineerId { get; set; }
-    public string Engineer {get; set; } // not sure if I should have this property and the engineers list property?  But I think it's right.
-    public List<Engineer> Engineers { get; set; } //? plural engineers? //just changed to list
-    public List<EnMa> JoinEntities { get;} //collection navigation property
-  }
+  // {
+  //   public int MachineId { get; set; }
+  //   [Required(ErrorMessage = "The machine's name can't be empty!")]
+  //   public string Name { get; set; }
+  //   [Range(1, int.MaxValue, ErrorMessage = "You must add your machine to a engineer. Have you created a engineer yet?")]
+  //   public int EngineerId { get; set; }
+  //   public string Engineer {get; set; } // not sure if I should have this property and the engineers list property?  But I think it's right.
+  //   public List<Engineer> Engineers { get; set; } //? plural engineers? //just changed to list
+  //   public List<EnMa> JoinEntities { get;} //collection navigation property
+  // }
 }
